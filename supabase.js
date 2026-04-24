b6b43413e36ca19ce0484831f415fddc2f496c5a
chashmishh <raunakgupta.personal@gmaill.com>
2026-04-24 09:14:19 +0530

    Update supabase client config

--- supabase.js
+++ supabase.js
@@ -1,5 +1,5 @@
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+const supabaseUrl = 'https://ftulccqzzuznrmcgam.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ0dWNsY3F6enV6Z25yaG1jZ2FtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY5NDIwNTUsImV4cCI6MjA5MjUxODA1NX0.WrM7Fv9jLwyEJSBUEw_bQjvX76kpgjtd5EtY7DoAey4'
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
