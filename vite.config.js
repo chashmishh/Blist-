7593f41ccfed1ba2d0185143eeb65deffe085881
chashmishh <raunakgupta.personal@gmaill.com>
2026-04-24 10:44:46 +0530

    Fixed Redirecting Issues

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,14 @@ import { defineConfig } from "vite";
 export default defineConfig({
   build: {
     target: "es2020",
+    rollupOptions: {
+      input: {
+        index: "index.html",
+        app: "app.html",
+        done: "done.html",
+        form: "form.html",
+      },
+    },
   },
 });
 
